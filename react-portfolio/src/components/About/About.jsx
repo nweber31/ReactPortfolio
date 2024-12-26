e691ea13fad09be763c8b06b8265c423110db6aa
Nathan Weber <nweber31@gatech.edu>
2024-12-26 10:39:04 -0700

    New images and icons

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -8,27 +8,27 @@ export const About = () => {
             <h2 className={styles.title}>About</h2>
             <div className={styles.content}>
                 <ul className={styles.aboutItems}>
-                    <li className={styles.aboutItem}><img src="/assets/about/cursorIcon.png" alt="CursorIcon" />
+                    <li className={styles.aboutItem}><img src="/assets/about/cursorIconNew1.png" alt="CursorIcon" />
                         <div className={styles.aboutItemText}>
                             <h3>Software Developer</h3>
                             <p>
-                                I am a software developer
+                                I am a software developer with experience in building responsive and optimized projects.
                             </p>
                         </div>
                     </li>
-                    <li className={styles.aboutItem}><img src="/assets/about/serverIcon.png" alt="ServerIcon" />
+                    <li className={styles.aboutItem}><img src="/assets/about/gameIcon1.png" alt="GameIcon" />
                         <div className={styles.aboutItemText}>
                             <h3>Game Developer</h3>
                             <p>
-                                I am a game developer
+                                I am a game developer with experience working in engines like Unity and Godot.
                             </p>
                         </div>
                     </li>
-                    <li className={styles.aboutItem}><img src="/assets/about/uiIcon.png" alt="uiIcon" />
+                    <li className={styles.aboutItem}><img src="/assets/about/musicIcon1.png" alt="MusicIcon" />
                         <div className={styles.aboutItemText}>
                             <h3>Musician</h3>
                             <p>
-                                I am a usician
+                                I am a musician with experience creating audio and music for games in FL Studio and Ableton.
                             </p>
                         </div>
                     </li>
--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
                 <p className={styles.description}> I recently graduated from Georgia Tech with a Bachelor's degree in Computational Media and a minor in Music Technology. I have a passion for music and videogames, and enjoy working together to find solutions to problems</p>
                 <a href="mailto:nweber31@gatech.edu" className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src="/assets/hero/headshot.png" alt="Headshot" className={styles.heroImg} />
+            <img src="/assets/hero/gradHeadshot.png" alt="Headshot" className={styles.heroImg} />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
