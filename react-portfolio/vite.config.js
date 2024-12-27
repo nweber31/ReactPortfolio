41736d5759a8ca3d6f721eff386b4750d4ea77a2
Nathan Weber <nweber31@gatech.edu>
2024-12-27 12:43:32 -0700

    Update vite.config.js

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ReactPortfolio/",
+  base: "/ReactPortfolio",
   plugins: [react()],
   css: {
     modules: {
