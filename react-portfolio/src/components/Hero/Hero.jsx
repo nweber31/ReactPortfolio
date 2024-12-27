4b2c2e805c83235dc1f7b144a2e6272017524dc1
Nathan Weber <nweber31@gatech.edu>
2024-12-27 11:32:49 -0700

    New Resume

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Nathan</h1>
-                <p className={styles.description}> I recently graduated from Georgia Tech with a Bachelor's degree in Computational Media and a minor in Music Technology. I have a passion for music and videogames, and enjoy working together to find solutions to problems.</p>
+                <p className={styles.description}> I recently graduated from Georgia Tech with a Bachelor's degree in Computational Media and a minor in Music Technology. I have a passion for videogames and music, and enjoy working together to find solutions to problems.</p>
                 <a href="mailto:nweber31@gatech.edu" className={styles.contactBtn}>Contact Me</a>
             </div>
             <img src="/assets/hero/gradHeadshot.png" alt="Headshot" className={styles.heroImg} />
--- react-portfolio/src/components/Resume/Resume.jsx
+++ react-portfolio/src/components/Resume/Resume.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Resume.module.css";
 
 export const Resume = () => {
-    const resumeUrl = "/assets/resume/Resume10-26-24.pdf";
+    const resumeUrl = "/assets/resume/WeberResume12-26-24.pdf";
     return (
         <div className={styles.container} id="resume">
             <h1 className={styles.title}>Resume</h1>
