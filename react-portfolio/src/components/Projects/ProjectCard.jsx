35545bf5237f9c5e7e2d2e18fe837632feca904e
Nathan Weber <nweber31@gatech.edu>
2024-12-30 16:44:57 -0700

    Project Image Test Again

--- react-portfolio/src/components/Projects/ProjectCard.jsx
+++ react-portfolio/src/components/Projects/ProjectCard.jsx
@@ -4,9 +4,10 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({ project: { title, imageSrc, description, demo, source } }) => {
+    const imagePath = new URL(`../../assets/${imageSrc}`, import.meta.url).href;
     return (
         <div className={styles.container}>
-            <img src={import.meta.env.BASE_URL + (imageSrc)} alt={title} className={styles.image} />
+            <img src={imagePath} alt={title} className={styles.image} />
             <h3 className={styles.title}>{title}</h3>
             <p className={styles.description}>{description}</p>
             <div className={styles.links}>
