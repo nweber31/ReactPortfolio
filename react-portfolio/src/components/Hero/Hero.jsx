06b057b0e46719f280c927cd3a6b49ea1058ab3d
Nathan Weber <nweber31@gatech.edu>
2024-12-30 16:12:27 -0700

    Update Hero.jsx

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Hero.module.css";
+import headshot from "/assets/hero/gradHeadshot.png";
 
 export const Hero = () => {
     return (
@@ -9,7 +10,7 @@ export const Hero = () => {
                 <p className={styles.description}> I recently graduated from Georgia Tech with a Bachelor's degree in Computational Media and a minor in Music Technology. I have a passion for videogames and music, and enjoy working together to find solutions to problems.</p>
                 <a href="mailto:nweber31@gatech.edu" className={styles.contactBtn}>Contact Me</a>
             </div>
-            <img src="/assets/hero/gradHeadshot.png" alt="Headshot" className={styles.heroImg} />
+            <img src={headshot} alt="Headshot" className={styles.heroImg} />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
