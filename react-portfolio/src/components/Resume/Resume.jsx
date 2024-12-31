e8131e118e79512f62a7620645d9a1c711da2c67
Nathan Weber <nweber31@gatech.edu>
2024-12-31 13:02:23 -0700

    Update Resume Link

--- react-portfolio/src/components/Resume/Resume.jsx
+++ react-portfolio/src/components/Resume/Resume.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import styles from "./Resume.module.css";
 
-import resume from "/assets/resume/WeberResume12-26-24.pdf";
+import resume from "/assets/resume/UpdatedResume-12-31-24.pdf";
 
 export const Resume = () => {
-    const resumeUrl = "/assets/resume/WeberResume12-26-24.pdf";
+    const resumeUrl = "/assets/resume/UpdatedResume-12-31-24.pdf";
     return (
         <div className={styles.container} id="resume">
             <h1 className={styles.title}>Resume</h1>
